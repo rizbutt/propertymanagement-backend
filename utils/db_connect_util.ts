1a0235ea19a27ab8078e760e921b1430b80833f2
Nouman-algo <nouman.khan@optimusalgos.com>
2024-08-20 18:00:53 +0500

    unwanted logs are removed

--- utils/db_connect_util.ts
+++ utils/db_connect_util.ts
@@ -13,21 +13,17 @@ if (!cached) {
 }
 
 async function dbConnect() {
-  console.log('dbConnect function called'); // Log when dbConnect is called
 
   if (cached.conn) {
-    console.log('Using cached connection');
     return cached.conn;
   }
 
   if (!cached.promise) {
-    console.log('Creating new connection promise');
     const opts = {
       bufferCommands: false,
     };
 
     cached.promise = mongoose.connect(MONGODB_URI as string, opts).then((mongoose) => {
-      console.log('Connected to MongoDB'); // Log when connected to MongoDB
       return mongoose;
     }).catch((err) => {
       console.error('Error connecting to MongoDB:', err); // Log errors
@@ -37,7 +33,6 @@ async function dbConnect() {
   }
 
   cached.conn = await cached.promise;
-  console.log('MongoDB connection established'); // Log when connection is established
   return cached.conn;
 }
 
